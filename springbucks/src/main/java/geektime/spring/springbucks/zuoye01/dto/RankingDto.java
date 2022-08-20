98cbbcd6d5ce5c9958e3d9265d0616b83dd086c0
liangmaokai <maokai.liang@pactera.com>
2022-08-20 18:13:54 +0800

    作业

---
+++ springbucks/src/main/java/geektime/spring/springbucks/zuoye01/dto/RankingDto.java
@@ -0,0 +1,28 @@
+package geektime.spring.springbucks.zuoye01.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Date: 2022/08/19/20:25
+ * @Description: 排名统计
+ */
+@Getter
+@Setter
+public class RankingDto {
+
+    /**
+     * 学生姓名
+     */
+    private String studentName;
+
+    /**
+     * 总分
+     */
+    private Double totalScore;
+
+    /**
+     * 排名
+     */
+    private Integer ranking;
+}
